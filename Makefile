aurutils/aurutils

commit d98e0fe239dc58cce8e4942f9bede943185e6ece

lib: formal split
This commit introduces a subdirectory structure for lib. There is no effect on
the installed package (the result of running the `Makefile`), and mainly serves
to improve overview of the different components. Every subdirectory can have
its own documentation with a `README.md` file.

Every subdirectory is self-contained; any programs within only relies on
programs from the same directory, or the AUR `perl` library. `aur-sync` uses
programs from all subdirectories, and is thus left in the main lib directory.

Makefile
@@ -9,7 +9,7 @@ AURUTILS_VERSION ?= $(shell git describe --tags || true)
 ifeq ($(AURUTILS_VERSION),)
 AURUTILS_VERSION := 20.1
 endif
-AURUTILS_SHELLCHECK = $(wildcard lib/*)
+AURUTILS_LIB = $(shell find lib/ -name 'aur-*')
 
 .PHONY: shellcheck install build completion aur
 
@@ -23,7 +23,7 @@ completion:
 	@$(MAKE) -C completions bash zsh
 
 shellcheck: aur
-	@shellcheck -x -f gcc -e 1071 $(AURUTILS_SHELLCHECK)
+	@shellcheck -x -f gcc -e 1071 $(AURUTILS_LIB)
 
 test: aur shellcheck
 	@tests/parseopt-consistency
@@ -33,7 +33,7 @@ install-aur: aur
 	@install -Dm755 aur       -t '$(DESTDIR)$(BINDIR)'
 
 install: install-aur
-	@install -Dm755 lib/aur-*  -t '$(DESTDIR)$(AURUTILS_LIB_DIR)'
+	@install -Dm755 $(AURUTILS_LIB) -t '$(DESTDIR)$(AURUTILS_LIB_DIR)'
 	@install -Dm644 man1/*     -t '$(DESTDIR)$(SHRDIR)/man/man1'
 	@install -Dm644 man7/*     -t '$(DESTDIR)$(SHRDIR)/man/man7'
 	@install -Dm644 LICENSE    -t '$(DESTDIR)$(SHRDIR)/licenses/$(PROGNM)'

commit d2bc7783a73aa24e4982f5761a539e6e3b260d7d

Fix bash completion
While at it, change 'AURUTILS_LIB' from a recursively expanded to
a simply expanded variable, and make the find command more explicit.

Fixes: d98e0fe2 (lib: formal split)
Co-authored-by: Earnestly <zibeon@gmail.com>

Makefile
@@ -9,7 +9,7 @@ AURUTILS_VERSION ?= $(shell git describe --tags || true)
 ifeq ($(AURUTILS_VERSION),)
 AURUTILS_VERSION := 20.5.1
 endif
-AURUTILS_LIB = $(shell find lib/ -name 'aur-*')
+AURUTILS_LIB := $(shell find lib/ -type f -name 'aur-*')
 
 .PHONY: shellcheck install build completion aur
 

completions/Makefile
@@ -8,9 +8,11 @@ ZSH_SITE_FUNCTIONS = \
 	zsh/_aur_packages \
 	zsh/_aur_repositories
 
+AURUTILS_LIB := $(shell find ../lib -type f -name 'aur-*')
+
 bash: bash/aur
 
-bash/aur: bash/aurutils.in ../lib/*
+bash/aur: bash/aurutils.in $(AURUTILS_LIB)
 	bash $< >$@
 
 install-bash: bash/aur

completions/command_opts.sh
@@ -8,7 +8,7 @@ default_opts() {
     local cmd corecommands=() opts=()
 
     for cmd in "${have_optdump[@]}"; do
-        mapfile -t opts < <(bash "../lib/aur-${cmd}" --dump-options | LC_ALL=C sort)
+        mapfile -t opts < <(find ../lib -type f -name "aur-$cmd" -exec bash -- {} --dump-options ';' | LC_ALL=C sort)
         corecommands+=("default_cmds[${cmd}]='${opts[*]}'")
     done
 